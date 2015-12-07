150e85e55a924d7fac0d6a3b9873cf62b498fad8
SDMS Student <sdmsstudent01@wethersfield.me>
2015-12-07 09:54:24 -0500

    Remove test students

--- HolidayTree/HolidayTree.ino
+++ HolidayTree/HolidayTree.ino
@@ -11,35 +11,4 @@ void loop(){}
 void setup() {
   Serial.begin(9600);
   tree.init(10);
-  elisa();
-}
-
-void elisa(){
-  tree.setRowColor(7, BLUE);
-  tree.setRowColor(6, WHITE);
-  tree.setRowColor(5, BLUE);
-  tree.setRowColor(4, WHITE);
-  tree.setRowColor(3, BLUE);
-  tree.setRowColor(2, WHITE);
-  tree.setRowColor(1, BLUE);
-  tree.setRowColor(0, WHITE);
-  tree.show(250);
-  tree.setRowColor(0, BLUE);
-  tree.setRowColor(1, WHITE);
-  tree.setRowColor(2, BLUE);
-  tree.setRowColor(3, WHITE);
-  tree.setRowColor(4, BLUE);
-  tree.setRowColor(5, WHITE);
-  tree.setRowColor(6, BLUE);
-  tree.setRowColor(7, WHITE);
-  tree.show(250);
-}
-
-void atif(){
-}
-
-void lynn(){
-}
-
-void jada(){
 }
