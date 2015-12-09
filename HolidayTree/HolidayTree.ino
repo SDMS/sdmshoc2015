8d7a81d609d761c368fbd9639ae4a892b5b22e80
Administrator <admin@MS-MBA-A3.local>
2015-12-09 16:18:07 -0500

    Elisa's Loop

--- HolidayTree/HolidayTree.ino
+++ HolidayTree/HolidayTree.ino
@@ -6,41 +6,71 @@
 
 HolidayTree tree = HolidayTree(16, 8);
 
-void loop(){}
+void loop(){
+  Elisac();
+}
 
 void setup() {
   Serial.begin(9600);
   tree.init(10);
-
-  Elisac();
 }
 
 
 void Elisac(){
-    for(int i = 0; i < 8; i++){
-  tree.setLightColor(i + 7,   i, MAGENTA);
-    }
-
-
-  Set_diagonal(2, WHITE);
-  Set_diagonal(1, RED);
-  Set_diagonal(0, YELLOW);
-  Set_diagonal(15, GREEN);
-  Set_diagonal(14, BLUE);
-  Set_diagonal(13, CYAN);
-  Set_diagonal(12, MAGENTA);
-  Set_diagonal(11, PURPLE);
-   tree.show(500);
-  tree.setColumnColor(9, RED);
+  tree.setColumnColor(0, RED);
+  tree.setColumnColor(1, RED);
+  tree.setColumnColor(2, RED);
+  tree.setColumnColor(3, RED);
+  tree.setColumnColor(4, RED);
+  tree.setColumnColor(5, WHITE);
+  tree.setColumnColor(6, BLUE);
+  tree.setColumnColor(7, BLUE);
+  tree.setColumnColor(8, BLUE);
+  tree.setColumnColor(9, BLUE);
+  tree.setColumnColor(10, BLUE);
+  tree.setColumnColor(11, GREEN);
+  tree.setColumnColor(12, GREEN);
+  tree.setColumnColor(13, GREEN); 
+  tree.setColumnColor(14, GREEN);
+  tree.setColumnColor(15, GREEN); 
+  tree.show(1000);
+  tree.setColumnColor(0, BLUE);
+  tree.setColumnColor(1, BLUE);
+  tree.setColumnColor(2, BLUE);
+  tree.setColumnColor(3, BLUE);
+  tree.setColumnColor(4, BLUE);
+  tree.setColumnColor(5, GREEN);
+  tree.setColumnColor(6, GREEN);
+  tree.setColumnColor(7, GREEN);
   tree.setColumnColor(8, GREEN);
-  tree.setColumnColor(7, CYAN);
+  tree.setColumnColor(9, GREEN);
+  tree.setColumnColor(10, WHITE);
+  tree.setColumnColor(11, RED);
+  tree.setColumnColor(12, RED);
+  tree.setColumnColor(13, RED);
+  tree.setColumnColor(14, RED);
+  tree.setColumnColor(15, RED);
+  tree.show(1000);
+  tree.setColumnColor(0, GREEN);
+  tree.setColumnColor(1, GREEN);
+  tree.setColumnColor(2, GREEN); 
+  tree.setColumnColor(3, GREEN);
+  tree.setColumnColor(4, GREEN);
+  tree.setColumnColor(5, RED);
   tree.setColumnColor(6, RED);
-  tree.show(500);
-
- }
+  tree.setColumnColor(7, RED);
+  tree.setColumnColor(8, RED);
+  tree.setColumnColor(9, RED); 
+  tree.setColumnColor(10, WHITE); 
+  tree.setColumnColor(11, BLUE);
+  tree.setColumnColor(12, BLUE);
+  tree.setColumnColor(13, BLUE); 
+  tree.setColumnColor(14, BLUE);
+  tree.setColumnColor(15, BLUE);
+  tree.show(1000);  
+}
 
 void Set_diagonal(int num, COLORS c){
- 
  for(int i = 0; i < 8; i++){
   tree.setLightColor(i + num,   i, c);
  }
